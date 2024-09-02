69c4577d2ec96958b47dba36ba6038dc27d1c822
Char <2103427676@qq.com>
2024-09-02 20:56:14 +0800

    29.Shader

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -7,6 +7,8 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Hazel/Renderer/Shader.h"
+
 namespace Hazel {
 	class HAZEL_API Application
 	{
@@ -30,6 +32,7 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::shared_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
