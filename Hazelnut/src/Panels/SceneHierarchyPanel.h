13407e7285474f3bed56e620b232090f137147e5
Char <2103427676@qq.com>
2024-12-23 15:00:33 +0800

    85.Properties panel

--- Hazelnut/src/Panels/SceneHierarchyPanel.h
+++ Hazelnut/src/Panels/SceneHierarchyPanel.h
@@ -16,6 +16,7 @@ namespace Hazel
 		void OnImGuiRender();
 	private:
 		void DrawEntityNode(Entity entity);
+		void DrawComponents(Entity entity);
 	private:
 		Ref<Scene> m_Context;
 		Entity m_SelectionContext;
