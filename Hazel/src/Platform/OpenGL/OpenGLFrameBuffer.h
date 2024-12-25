24916fbd5d277c2bdc08eaaafb73c87791204798
Char <2103427676@qq.com>
2024-12-25 18:59:37 +0800

    95.Add MRT to Framebuffer

--- Hazel/src/Platform/OpenGL/OpenGLFrameBuffer.h
+++ Hazel/src/Platform/OpenGL/OpenGLFrameBuffer.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "Hazel/Renderer/Framebuffer.h"
 
-namespace Hazel {
+namespace Hazel
+{
 	class OpenGLFramebuffer : public Framebuffer
 	{
 	public:
@@ -13,11 +14,15 @@ namespace Hazel {
 
 		virtual void Resize(uint32_t width, uint32_t height) override;
 
-		virtual uint32_t GetColorAttachmentRendererID() const override { return m_ColorAttachment; }
+		virtual uint32_t GetColorAttachmentRendererID(uint32_t index = 0) const override { HZ_CORE_ASSERT(index < m_ColorAttachments.size(), ""); return m_ColorAttachments[index]; }
 		virtual const FramebufferSpecification& GetSpecification() const override { return m_Specification; }
 	private:
 		uint32_t m_RendererID = 0; // FBO标识符
-		uint32_t m_ColorAttachment = 0, m_DepthAttachment = 0; // 颜色纹理和深度纹理
 		FramebufferSpecification m_Specification;
+
+		std::vector<FramebufferTextureSpecification> m_ColorAttachmentSpecifications;
+		FramebufferTextureSpecification m_DepthAttachmentSpecification = FramebufferTextureFormat::None;
+		std::vector<uint32_t> m_ColorAttachments;
+		uint32_t m_DepthAttachment = 0;
 	};
 }
