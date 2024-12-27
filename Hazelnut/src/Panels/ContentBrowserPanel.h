22d65393385e88140027b1ec67192d6d84aaf37f
Char <2103427676@qq.com>
2024-12-27 20:02:57 +0800

    102.assets browser thumbnail

--- Hazelnut/src/Panels/ContentBrowserPanel.h
+++ Hazelnut/src/Panels/ContentBrowserPanel.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include "Hazel/Renderer/Texture.h"
 
 namespace Hazel
 {
@@ -11,5 +12,7 @@ namespace Hazel
 		void OnImGuiRender();
 	private:
 		std::filesystem::path m_CurrentDirectory;
+		Ref<Texture2D> m_DirectoryIcon;
+		Ref<Texture2D> m_FileIcon;
 	};
 }
